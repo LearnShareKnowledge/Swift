0daa284356dcc2ae5b4be3bb6cd16d1484bd3850
Anukool Tiwari <anukool.tiwari@philips.com>
2017-06-05 18:55:49 +0530

    This commit has - 1. programatically creating a textview and setting up the parameters 2. adding the KDCircularProgressBar and setting up the parameters

--- PlayingWithTextview/ViewController.swift
+++ PlayingWithTextview/ViewController.swift
@@ -8,18 +8,58 @@
 
 import UIKit
 
+import KDCircularProgress
+
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var tvTextView : UITextView!
+    
+    var hydrationMeasurementView : KDCircularProgress!
+    
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad() ;
+        
+        tvTextView = UITextView(frame: CGRect(x: 0, y: 0, width: 300, height: 300)) ;
+        
+        tvTextView.center = self.view.center ;
+        
+        tvTextView.backgroundColor = UIColor.gray ;
+        
+        tvTextView.textColor = UIColor.white ;
+        
+        tvTextView.text = "Hello , i have been added dynamically"
+        
+        tvTextView.font = UIFont.boldSystemFont(ofSize: 20) ;
+        
+        tvTextView.textAlignment = .justified ;
+        
+        self.view.addSubview(tvTextView) ;
+        
+        //initialize the circular progress bar 
+        
+        hydrationMeasurementView = KDCircularProgress(frame: CGRect(x :0 ,y : 0, width : 300 , height : 300)) ;
+        
+        hydrationMeasurementView.center = self.view.center ;
+        
+        hydrationMeasurementView.trackThickness = 0.2 ;
+        
+        hydrationMeasurementView.trackColor = UIColor(red : 1.0 , green : 1.0 , blue : 1.0 , alpha : 1.0) ;
+        
+        self.view.addSubview(hydrationMeasurementView) ;
+        
+        
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
